d58060f167a38647b306a23712acb2ffaf37db47
niskylark11 <50236051+niskylark11@users.noreply.github.com>
2019-11-11 17:24:02 +0700

    03-G - Validasi Email - Halaman Register

--- proses_register.php
+++ proses_register.php
@@ -16,12 +16,19 @@
     unset($_POST['re_password']);
     $dataForm = http_build_query($_POST);
 
+    $query = mysqli_query($koneksi, "SELECT * FROM user WHERE email='$email'");
+
     if(empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password)){
         header("location: ".BASE_URL."index.php?page=register&notif=require&$dataForm");  //jika salah satu data kosong, maka akan dikembalikan ke halaman register//
     }elseif ($password != $re_password){
         header("location: ".BASE_URL."index.php?page=register&notif=password&$dataForm"); //jika password dan re_password tidak sama, maka akan dikembalikan ke halaman register//
+    }elseif (mysqli_num_rows($query) == 1){
+            header("location: ".BASE_URL."index.php?page=register&notif=email&$dataForm");
     }
     else{
-    mysqli_query($koneksi, "INSERT INTO user (level, nama, email, alamat, phone, password, status)
+        $password = md5 ($password);
+        mysqli_query($koneksi, "INSERT INTO user (level, nama, email, alamat, phone, password, status)
                                         VALUES ('$level', '$nama_lengkap', '$email', '$alamat', '$phone', '$password', '$status')");
+        
+        header("location: ".BASE_URL."index.php?page=login");
     }
