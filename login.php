5925313ebdcfb451daa2bb651348d360b26c685b
niskylark11 <50236051+niskylark11@users.noreply.github.com>
2019-11-13 10:52:03 +0700

    04-B - Proses Login - Halaman Login

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
         <?php
             $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
 
-            if($notif == "require"){
+            if($notif == true){
                 echo "<div class='notif'>Maaf, email atau password yang kamu masukan tidak cocok</div>";
             }
         ?>
@@ -14,7 +14,7 @@
             <label>Email</label>
             <span><input type="text" name="email" /></span>
         </div>
-        
+
         <div class="element-form">
             <label>Password</label>
             <span><input type="password" name="password" /></span>
