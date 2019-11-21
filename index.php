527e27e1d323d15958e2f937f9217e2c1c720804
niskylark11 <50236051+niskylark11@users.noreply.github.com>
2019-11-21 14:00:00 +0700

    04-F - Logout - Halaman Logout

--- index.php
+++ index.php
@@ -33,7 +33,9 @@
                 <div id="user">
                     <?php
                         if($user_id){
-                            echo "Hi <b>$nama</b>, <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile</a>";
+                            echo "Hi <b>$nama</b>,
+                                  <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile</a>
+                                  <a href='".BASE_URL."logout.php'>Logout</a>";
                         }else{
                             echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
                                  <a href='".BASE_URL."index.php?page=register'>Register</a>";
