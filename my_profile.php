44e60140c1777388841b7e25593e09016bc6e369
niskylark11 <50236051+niskylark11@users.noreply.github.com>
2019-11-23 13:07:57 +0700

    05-C - Proteksi Login dan Module Menu - My Profile

--- my_profile.php
+++ my_profile.php
@@ -1,3 +1,14 @@
+<?php
+
+    if($user_id){
+        $module = isset($_GET['module']) ? $_GET['module'] : false;
+        $action = isset($_GET['action']) ? $_GET['action'] : false;
+        $mode = isset($_GET['mode']) ? $_GET['mode'] : false;
+    }else{
+        header("location: ".BASE_URL."index.php?page=login");
+    }
+
+?>
 <div id="bg-page-profile">
 
     <div id="menu-profile">
@@ -26,7 +37,16 @@
     </div>
 
     <div id="profile-content">
-        Belum ada data
+        <?php
+
+        $file = "module/$module/$action.php";
+        if(file_exists($file)){
+            include_once($file);
+        }else{
+            echo "<h3> Maaf, halaman tersebut tidak ditemukan</h3>";
+        }
+
+        ?>
     </div>
 
 </div>
